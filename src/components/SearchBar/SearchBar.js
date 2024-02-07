d361ee9c93e3d435d0969b18164549193f7c8026
Caleb Hagood <calebbzhagood@gmail.com>
2024-02-07 16:59:36 -0500

    updated SearchBar css and connected css file to SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import styles from './SearchBar.module.css'
 
 function SearchBar(){
     return (
-        <div>
-            <h2 id="heroText">Playlisting Made Easy.</h2>
-            <form action="#" id="search">
-                <input type="text" id="searchBar" />
+        <div className={styles.div}>
+            <h2 className={styles.heroText} id="heroText">Playlisting Made Easy.</h2>
+            <form className={styles.form} action="#" id="search">
+                <input className={styles.searchBar} type="text" id="searchBar" placeholder="Song Title" />
                 <button id="searchButton">Search Songs</button>
             </form>
         </div>
